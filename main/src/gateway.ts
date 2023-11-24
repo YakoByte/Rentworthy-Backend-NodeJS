4427500452c40470064c7ce1c71bc6632c6900ab
Jacob-alwin <alwinjacobjoy@gmail.com>
2023-11-25 02:09:03 +0530

    Docker Setup

--- main/src/gateway.ts
+++ main/src/gateway.ts
@@ -1,57 +1,78 @@
-import * as httpProxy from 'http-proxy';
-import { IncomingMessage, ServerResponse } from 'http';
+import * as httpProxy from "http-proxy";
+import { IncomingMessage, ServerResponse } from "http";
 
 const httpProxyAny = httpProxy as any;
 const gateway = httpProxyAny.createProxyServer({});
-const chatServiceProxy = httpProxyAny.createProxyServer({ target: 'http://localhost:5006' });
-gateway.on('error', (err: Error, req: IncomingMessage, res: ServerResponse) => {
-  console.error('Error occurred while proxying:', err);
+const chatServiceProxy = httpProxyAny.createProxyServer({
+  target: "http://localhost:5006",
+});
+gateway.on("error", (err: Error, req: IncomingMessage, res: ServerResponse) => {
+  console.error("Error occurred while proxying:", err);
   res.writeHead(500, {
-    'Content-Type': 'text/plain'
+    "Content-Type": "text/plain",
   });
-  res.end('Proxy error: ' + err.message);
+  res.end("Proxy error: " + err.message);
 });
 
-const userMicroservice: string = 'http://localhost:5001';
-const categoryMicroservice: string = 'http://localhost:5002';
-const uploadMicroservice: string = 'http://localhost:5003';
-const productMicroservice: string = 'http://localhost:5004';
-const rentingMicroservice: string = 'http://localhost:5005';
-const chatMicroservice: string = 'http://localhost:5006';
+const userMicroservice: string =
+  // process.env.USER_MICROSERVICE_URL ||
+  "http://user:5001";
 
-// user microservice
+// const categoryMicroservice: string = "http://localhost:5002";
+// const uploadMicroservice: string = "http://localhost:5003";
+// const productMicroservice: string = "http://localhost:5004";
+// const rentingMicroservice: string = "http://localhost:5005";
+// const chatMicroservice: string = "http://localhost:5006";
+const categoryMicroservice: string =
+  // process.env.CATEGORY_MICROSERVICE_URL ||
+  "http://category:5002";
+const uploadMicroservice: string =
+  // process.env.UPLOAD_MICROSERVICE_URL ||
+  "http://upload:5003";
+const productMicroservice: string =
+  // process.env.PRODUCT_MICROSERVICE_URL ||
+  "http://product:5004";
+const rentingMicroservice: string =
+  // process.env.RENTING_MICROSERVICE_URL ||
+  "http://renting:5005";
+const chatMicroservice: string =
+  // process.env.CHAT_MICROSERVICE_URL ||
+  "http://chat:5006";
+
+// User microservice
 const user = (req: IncomingMessage, res: ServerResponse) => {
-  console.log('Routing to user microservice', req.url);
+  console.log("Routing to user microservice", req.url);
   gateway.web(req, res, { target: userMicroservice });
 };
 
-// category microservice
+// Category microservice
 const category = (req: IncomingMessage, res: ServerResponse) => {
-  console.log('Routing to category microservice', req.url);
+  console.log("Routing to category microservice", req.url);
   gateway.web(req, res, { target: categoryMicroservice });
 };
 
+// Upload microservice
 const upload = (req: IncomingMessage, res: ServerResponse) => {
-  console.log('Routing to upload microservice', req.url);
+  console.log("Routing to upload microservice", req.url);
   gateway.web(req, res, { target: uploadMicroservice });
 };
 
-// product microservice
+// Product microservice
 const product = (req: IncomingMessage, res: ServerResponse) => {
-  console.log('Routing to product microservice', req.url);
+  console.log("Routing to product microservice", req.url);
   gateway.web(req, res, { target: productMicroservice });
 };
 
-// renting microservice
+// Renting microservice
 const renting = (req: IncomingMessage, res: ServerResponse) => {
-  console.log('Routing to renting microservice', req.url);
+  console.log("Routing to renting microservice", req.url);
   gateway.web(req, res, { target: rentingMicroservice });
 };
 
-// chat microservice
+// Chat microservice
 const chat = (req: IncomingMessage, res: ServerResponse) => {
-  console.log('Routing to chat microservice', req.url);
-  chatServiceProxy.ws(req, res);
+  console.log("Routing to chat microservice", req.url);
+  gateway.web(req, res, { target: chatMicroservice });
 };
 
 export { user, category, upload, product, renting, chat };
--- main/src/index.ts
+++ main/src/index.ts
@@ -1,46 +1,49 @@
-import express, { Express, Request, Response } from 'express';
-import { PORT } from './config';
-import { product, user, category, upload, renting, chat } from './gateway';
-import path from 'path';
+import express, { Express, Request, Response } from "express";
+import { PORT } from "./config";
+import { product, user, category, upload, renting, chat } from "./gateway";
+import path from "path";
 
 const StartServer = async (): Promise<void> => {
   const app: Express = express();
 
-  app.get('/', (req: Request, res: Response) => {
+  app.get("/", (req: Request, res: Response) => {
     res.status(200).send({ message: "Microservices called........" });
   });
 
   // Use the reverse proxy server defined in the gateway module
-  app.use('/app/api/v1/user', user);
-  app.use('/web/api/v1/user', user);
-  app.use('/web/api/v1/category', category);
-  app.use('/app/api/v1/category', category);
-  app.use('/web/api/v1/upload', upload)
-  app.use('/app/api/v1/upload', upload)
-  app.use('/web/api/v1/product', product)
-  app.use('/app/api/v1/product', product)
-  app.use('/web/api/v1/renting', renting)
-  app.use('/app/api/v1/renting', renting)
+  app.use("/app/api/v1/user", user);
+  app.use("/web/api/v1/user", user);
+  app.use("/web/api/v1/category", category);
+  app.use("/app/api/v1/category", category);
+  app.use("/web/api/v1/upload", upload);
+  app.use("/app/api/v1/upload", upload);
+  app.use("/web/api/v1/product", product);
+  app.use("/app/api/v1/product", product);
+  app.use("/web/api/v1/renting", renting);
+  app.use("/app/api/v1/renting", renting);
   // app.use('/web/api/v1/chat', chat)
   // app.use('/app/api/v1/chat', chat)
 
-
   // 10mb limit for file upload
-  app.use(express.json({ limit: '10mb' }));
-  app.use(express.static(path.join(__dirname, '../../public'), {
-    maxAge: '1d',
-    etag: true,
-    lastModified: true
-  }));
-  app.set('uploads', path.join(__dirname, '../../public'));
+  app.use(express.json({ limit: "10mb" }));
+  app.use(
+    express.static(path.join(__dirname, "../../public"), {
+      maxAge: "1d",
+      etag: true,
+      lastModified: true,
+    })
+  );
+  app.set("uploads", path.join(__dirname, "../../public"));
 
   // server listening
-  app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-  }).on('error', (err) => {
-    console.log(err);
-    process.exit(1);
-  });
-}
+  app
+    .listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`);
+    })
+    .on("error", (err) => {
+      console.log(err);
+      process.exit(1);
+    });
+};
 
 StartServer();
