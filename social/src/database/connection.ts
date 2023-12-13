c4afc4b41984eec429d0ed2f5f1b5ab1a93eeea9
MAYANK SINGH <mayanksinghms8055@gmail.com>
2023-12-13 19:24:58 +0530

    user

--- social/src/database/connection.ts
+++ social/src/database/connection.ts
@@ -1,18 +1,35 @@
-import mongoose, { ConnectOptions } from 'mongoose';
-import { DATABASE } from '../config';
+// import mongoose, { ConnectOptions } from 'mongoose';
+// import { DATABASE } from '../config';
 
 // interface MongooseConnectionOptions {
 //     useNewUrlParser: boolean;
 //     useUnifiedTopology: boolean;
 // }
 
-const mongooseOptions: ConnectOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-};
+// const mongooseOptions: ConnectOptions = {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// };
+// const databaseConnection = async (): Promise<void> => {
+//     try {
+//         await mongoose.connect(DATABASE, mongooseOptions);
+//         console.log('MongoDB connected successfully');
+//     } catch (error) {
+//         console.error('Error connecting to MongoDB:', error);
+//         process.exit(1);
+//     }
+// };
+
+// export default databaseConnection;
+
+
+
+import mongoose from 'mongoose';
+import { DATABASE } from '../config';
+
 const databaseConnection = async (): Promise<void> => {
     try {
-        await mongoose.connect(DATABASE, mongooseOptions);
+        await mongoose.connect(DATABASE);
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
@@ -21,6 +38,3 @@ const databaseConnection = async (): Promise<void> => {
 };
 
 export default databaseConnection;
-
-
-
