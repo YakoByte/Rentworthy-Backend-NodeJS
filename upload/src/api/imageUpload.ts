c66b728cdcac383b97ca52a77f6b8921c78c4a83
bhavdip01 <bhavdip.gadara762@gmail.com>
2023-12-23 14:52:21 +0530

    update upload service

--- upload/src/api/imageUpload.ts
+++ upload/src/api/imageUpload.ts
@@ -10,12 +10,12 @@ export default (app: Express) => {
     const service = new ImageService();
 
     // API = create new image-upload
-    app.post('/image-upload', UserAuth, upload.single("image"), isAdmin, async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    app.post('/image-upload', UserAuth, upload.single("image"), async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
         try {
             let authUser: any = req.user
             req.body.userId = authUser._id;
             req.body.imageDetail = req.file;
-            // console.log("req.body", req.body)
+            console.log("req.file", req.file)
             const { data } = await service.CreateImage(req.body);
             return res.json(data);
         } catch (err) {
@@ -41,7 +41,7 @@ export default (app: Express) => {
     app.delete('/image-delete/:id', UserAuth, isAdmin, async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
         try {
             console.log(req.params.id);
-            
+
             const { data } = await service.DeleteImage(req.params.id);
             return res.json(data);
         } catch (err) {
--- upload/src/config/index.ts
+++ upload/src/config/index.ts
@@ -5,7 +5,11 @@ export const PORT: string | undefined = process.env.PORT;
 export const DATABASE: string = "mongodb+srv://ecommerce:rejoice123@cluster0.ooqoe24.mongodb.net/rent?retryWrites=true&w=majority";
 export const SECRET_KEY: string | undefined = "YAKOBYTE";
 
-export const AWS_ACCESS_KEY_ID: string | undefined = process.env.AWS_ACCESS_KEY_ID;
-export const AWS_SECRET_ACCESS_KEY: string | undefined = process.env.AWS_SECRET_ACCESS_KEY;
-export const AWS_BUCKET_REGION: string | undefined = process.env.AWS_BUCKET_REGION;
-export const AWS_BUCKET_NAME: string | undefined = process.env.AWS_BUCKET_NAME;
+// export const AWS_ACCESS_KEY_ID: string | undefined = "process.env.AWS_ACCESS_KEY_ID";
+export const AWS_ACCESS_KEY_ID: string | undefined = "AKIAYMJZXRWEBBSQZF52";
+// export const AWS_SECRET_ACCESS_KEY: string | undefined = process.env.AWS_SECRET_ACCESS_KEY;
+export const AWS_SECRET_ACCESS_KEY: string | undefined = "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";
+// export const AWS_BUCKET_REGION: string | undefined = process.env.AWS_BUCKET_REGION;
+export const AWS_BUCKET_REGION: string | undefined = "ap-south-1";
+// export const AWS_BUCKET_NAME: string | undefined = process.env.AWS_BUCKET_NAME;
+export const AWS_BUCKET_NAME: string | undefined = "yako-mayank";
