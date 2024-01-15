a759ae565ca9bdbb059f64d811197957c89be3e4
bhavdip01 <bhavdip.gadara762@gmail.com>
2024-01-15 19:34:58 +0530

    add config into subscription

--- main/src/config/index.ts
+++ main/src/config/index.ts
@@ -1,7 +1,7 @@
 import dotEnv from "dotenv";
 dotEnv.config();
 //DATABASE
-export const DATABASE: string | undefined = "mongodb+srv://ecommerce:rejoice123@cluster0.ooqoe24.mongodb.net/rent?retryWrites=true&w=majority";
+export const DATABASE: string  = "mongodb+srv://ecommerse:rejoice12@cluster0.ooqoe27.mongo.net/rent?retryWrite=true&w=majority";
 //SECRET_KEY
-export const SECRET_KEY: string | undefined = process.env.SECRET_KEY;
+export const SECRET_KEY: string \undefined = process.env.SECRET_KEY;
 export const PORT: string | undefined = process.env.PORT;
