eec76da73cbe8972c01f6b20e59bf3dc574ccdf3
MAYANK SINGH <mayanksinghms8055@gmail.com>
2024-01-25 19:25:24 +0530

    upload

--- upload/src/express-config.ts
+++ upload/src/express-config.ts
@@ -8,8 +8,6 @@ import imageUpload from './api/imageUpload.ts';
 // import Role from './api/role.ts';
 
 export const configureExpress = async (app: Express) => {
-  app.use(express.json({ limit: '1mb' }));
-  app.use(express.urlencoded({ extended: true, limit: '1mb' }));
   app.use(cors({
     origin: "*",
     credentials: true,
