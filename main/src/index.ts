63ff53f931b29a8d9834efef1b327bdc36419fd0
MAYANK SINGH <mayanksinghms8055@gmail.com>
2024-02-12 11:31:05 +0530

    user

--- main/src/index.ts
+++ main/src/index.ts
@@ -39,7 +39,7 @@ const StartServer = async (): Promise<void> => {
     // Middleware
     app.use(helmet());
     app.use(cors());
-    app.use(express.json({ limit: "10mb" }));
+    // app.use(express.json());
     app.use(ValidateKey);
 
     // Serve static files
