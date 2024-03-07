1483032064f0192483c451ec1a3ae1b30f0dfa86
MAYANK SINGH <mayanksinghms8055@gmail.com>
2024-03-07 10:47:28 +0530

    07-03-2024

--- product/src/database/repository/product.ts
+++ product/src/database/repository/product.ts
@@ -125,154 +125,144 @@ class ProductRepository {
   //get product by id
   async getProductByUserId(productInputs: { userId: string }) {
     try {
-      const findProduct = await productModel.aggregate([
-        {
-          $match: {
-            userId: new Types.ObjectId(productInputs.userId),
-            isDeleted: false,
-            isActive: true,
-          },
-        },
-        {
-          $lookup: {
-            from: "images",
-            localField: "images",
-            foreignField: "_id",
-            pipeline: [{ $project: { _id: 1, mimetype: 1, path: 1, imageName: 1, size: 1, userId: 1 } }],
-            as: "images",
-          },
-        },
-        {
-          $lookup: {
-            from: "users",
-            localField: "userId",
-            foreignField: "_id",
-            pipeline: [
-              { $project: { _id: 1, email: 1, phoneNo: 1, roleId: 1, bussinessType: 1, loginType: 1 } },
-            ],
-            as: "userId",
-          },
-        },
-      ]);
-
-      if (findProduct.length === 0) {
-        return { message: "Product not found" };
-      }
-
-      const wishlistPromises: any = await Promise.all(
-        findProduct.map(async (element) => {          
-          let profileData = await ProfileModel.aggregate([
+        const findProduct = await productModel.aggregate([
             {
-              $match: {
-                userId: element.userId[0]._id
-              },
-            },
-            {
-              $lookup: {
-                from: "images",
-                localField: "profileImage",
-                foreignField: "_id",
-                pipeline: [
-                  { $project: { _id: 1, mimetype: 1, path: 1, imageName: 1, size: 1, userId: 1 } }
-                ],
-                as: "profileImage",
-              },
-            },
-          ]);
-          if(profileData.length > 0 && profileData[0].profileImage.length > 0 && profileData[0].profileImage[0].imageName) {
-            element.userId[0].profile = await generatePresignedUrl(profileData[0].profileImage[0].imageName);
-            element.userId[0].userName = profileData[0].userName
-          }
-
-          const productLike = await productLikeModel.countDocuments({
-            productId: element._id,
-            isFav: true,
-            isDeleted: false,
-          });
-          element.productLike = productLike;
-
-          element.images.forEach(async(element: any) => {
-            let newPath = await generatePresignedUrl(element.imageName);
-            element.path = newPath;
-          });
-          try {
-            const today = new Date();
-            today.setHours(0, 0, 0, 0);
-            let productBooking = await Bookings.find({
-              productId: element._id,
-            }).select({
-              _id: 1,
-              startDate: 1,
-              endDate: 1,
-              quantity: 1,
-              status: 1,
-            });
-
-            let wishlistData = await Wishlists.aggregate([
-              {
-                $match: { productIds: { "$in": element._id } }
-              },
-              {
-                $lookup: {
-                  from: "users",
-                  localField: "userId",
-                  foreignField: "_id",
-                  pipeline: [
-                    { $project: { _id: 1, email: 1, phoneNo: 1, roleId: 1, bussinessType: 1, loginType: 1 } },
-                  ],
-                  as: "userId",
-                },
-              },
-            ]);
-
-            wishlistData.forEach(async(element) => {
-            let wishlistProfileData = await ProfileModel.aggregate([
-              {
                 $match: {
-                  userId: element.userId[0]._id
+                    userId: new Types.ObjectId(productInputs.userId),
+                    isDeleted: false,
+                    isActive: true,
                 },
-              },
-              {
+            },
+            {
                 $lookup: {
-                  from: "images",
-                  localField: "profileImage",
-                  foreignField: "_id",
-                  pipeline: [
-                    { $project: { _id: 1, mimetype: 1, path: 1, imageName: 1, size: 1, userId: 1 } }
-                  ],
-                  as: "profileImage",
+                    from: "images",
+                    localField: "images",
+                    foreignField: "_id",
+                    pipeline: [{ $project: { _id: 1, mimetype: 1, path: 1, imageName: 1, size: 1, userId: 1 } }],
+                    as: "images",
                 },
-              },
-            ]);
-            if(wishlistProfileData.length > 0 && wishlistProfileData[0].profileImage.length > 0 && wishlistProfileData[0].profileImage[0].imageName) {
-              element.userId[0].profile = await generatePresignedUrl(wishlistProfileData[0].profileImage[0].imageName);
-              element.userId[0].userName = wishlistProfileData[0].userName
-            }
-          });
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "userId",
+                    foreignField: "_id",
+                    pipeline: [
+                        { $project: { _id: 1, email: 1, phoneNo: 1, roleId: 1, businessType: 1, loginType: 1 } },
+                    ],
+                    as: "userId",
+                },
+            },
+        ]);
 
-            return { product: element, wishlistData, productBooking };
-          } catch (error) {
-            console.error(
-              `Error processing wishlist for product ${element._id}: ${error}`
-            );
-            // Handle the error as needed
-            return {
-              product: element,
-              wishlistData: null,
-              productBooking: [],
-            };
-          }
-        })
-      );
+        if (findProduct.length === 0) {
+            return { message: "Product not found" };
+        }
 
-      return {
-        data: await Promise.all(wishlistPromises),
-      };
+        const wishlistPromises = await Promise.all(
+            findProduct.map(async (element) => {
+                let profileData = await ProfileModel.aggregate([
+                    {
+                        $match: {
+                            userId: element.userId[0]._id,
+                        },
+                    },
+                    {
+                        $lookup: {
+                            from: "images",
+                            localField: "profileImage",
+                            foreignField: "_id",
+                            pipeline: [
+                                { $project: { _id: 1, mimetype: 1, path: 1, imageName: 1, size: 1, userId: 1 } },
+                            ],
+                            as: "profileImage",
+                        },
+                    },
+                ]);
+                if (profileData.length > 0 && profileData[0].profileImage.length > 0 && profileData[0].profileImage[0].imageName) {
+                    element.userId[0].profile = await generatePresignedUrl(profileData[0].profileImage[0].imageName);
+                    element.userId[0].userName = profileData[0].userName;
+                }
+
+                const productLike = await productLikeModel.countDocuments({
+                    productId: element._id,
+                    isFav: true,
+                    isDeleted: false,
+                });
+                element.productLike = productLike;
+
+                await Promise.all(element.images.map(async (img: any) => {
+                    let newPath = await generatePresignedUrl(img.imageName);
+                    img.path = newPath;
+                }));
+
+                const today = new Date();
+                today.setHours(0, 0, 0, 0);
+                const productBooking = await Bookings.find({
+                    productId: element._id,
+                }).select({
+                    _id: 1,
+                    startDate: 1,
+                    endDate: 1,
+                    quantity: 1,
+                    status: 1,
+                });
+
+                const wishlistData = await Wishlists.aggregate([
+                    {
+                        $match: { productIds: { $in: [element._id] } }
+                    },
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "userId",
+                            foreignField: "_id",
+                            pipeline: [
+                                { $project: { _id: 1, email: 1, phoneNo: 1, roleId: 1, businessType: 1, loginType: 1 } },
+                            ],
+                            as: "userId",
+                        },
+                    },
+                ]);
+
+                await Promise.all(wishlistData.map(async (wishlistElement) => {
+                    let wishlistProfileData = await ProfileModel.aggregate([
+                        {
+                            $match: {
+                                userId: wishlistElement.userId[0]._id,
+                            },
+                        },
+                        {
+                            $lookup: {
+                                from: "images",
+                                localField: "profileImage",
+                                foreignField: "_id",
+                                pipeline: [
+                                    { $project: { _id: 1, mimetype: 1, path: 1, imageName: 1, size: 1, userId: 1 } },
+                                ],
+                                as: "profileImage",
+                            },
+                        },
+                    ]);
+                    if (wishlistProfileData.length > 0 && wishlistProfileData[0].profileImage.length > 0 && wishlistProfileData[0].profileImage[0].imageName) {
+                        wishlistElement.userId[0].profile = await generatePresignedUrl(wishlistProfileData[0].profileImage[0].imageName);
+                        wishlistElement.userId[0].userName = wishlistProfileData[0].userName;
+                    }
+                }));
+
+                return { product: element, wishlistData, productBooking };
+            })
+        );
+
+        return {
+            data: wishlistPromises,
+        };
     } catch (err) {
-      console.log("error", err);
-      throw new Error("Unable to Get Product");
+        console.error("Error:", err);
+        throw new Error("Unable to Get Product");
     }
-  }
+}
+
 
   //get product by id
   async getProductById(productInputs: { _id: string; userId: string }) {
