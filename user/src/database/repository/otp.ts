6f0c074b5e85dbe0c5e41ebe2fb4ee7023d0e573
MAYANK SINGH <mayanksinghms8055@gmail.com>
2024-04-15 13:57:35 +0530

    15-04-2024

--- user/src/database/repository/otp.ts
+++ user/src/database/repository/otp.ts
@@ -7,7 +7,7 @@ import { sendSMS } from "../../template/smsTemplate";
 class OTPRepository {
   //create otp
   async CreateOTP(otpInputs: otpRequest) {
-    try {      
+    try {            
       // Check 5 times to get OTP from the same IP and (email or phoneNo)
       const findOTP = await otpModel.find({
         ipAddress: otpInputs.ipAddress,
@@ -99,7 +99,7 @@ class OTPRepository {
 
   //verify otp
   async VerifyOTP(otpInputs: otpRequest) {
-    try {
+    try {      
       const existingOTP: any = await otpModel.findOne({
         ipAddress: otpInputs.ipAddress,
         $or: [{ email: otpInputs.email }, { phoneNo: otpInputs.phoneNo }],
